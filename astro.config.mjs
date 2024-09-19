c344564c958efd11cb6c5f3bcd2467012b0d0d3e
alvarosh15 <alvaro.s.h.15@gmail.com>
2024-09-19 20:05:11 +0200

    Trying to fix images not loading on Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/static";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: vercel({
-    imageService: true,
-  }),
+  output: "server",
+  adapter: vercel(),
 });
