c6e7de872d50d1e455cb178d8c0c4ffa894dda6d
alvarosh15 <alvaro.s.h.15@gmail.com>
2026-04-27 21:02:18 +0200

    feat: animated cursor

---
+++ src/constants/cursor-icons.ts
@@ -0,0 +1,7 @@
+export const CursorIcon = {
+  Github: "github",
+  LinkedIn: "linkedin",
+  Mail: "mail",
+  X: "x",
+  External: "external",
+} as const;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,11 +3,12 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {},
-    colors: {
-      "light-brown-bg": "#fffdfa",
-      "dark-brown-bg": "#f4f0e7",
-      "dark-brown-text": "#a69b85",
+    extend: {
+      colors: {
+        "light-brown-bg": "#fffdfa",
+        "dark-brown-bg": "#f4f0e7",
+        "dark-brown-text": "#a69b85",
+      },
     },
   },
   plugins: [],
