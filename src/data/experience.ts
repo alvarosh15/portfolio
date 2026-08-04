eb89893783fdd51cb3a2973cc1644ee5f37c183d
alvarosh15 <alvaro.s.h.15@gmail.com>
2026-08-04 22:47:06 +0200

    Improve portfolio content and accessibility

---
+++ src/data/experience.ts
@@ -0,0 +1,32 @@
+export const experience = [
+  {
+    date: "January 2025 - Present",
+    title: "Software Engineer - Google / VirusTotal",
+  },
+  {
+    date: "September 2024 - December 2024",
+    title: "Junior Software Engineer - Indra Espacio",
+    description:
+      "Developing and maintaining full-stack applications using React and Django.",
+  },
+  {
+    date: "October 2024 - December 2024",
+    title:
+      "VI University Expert Diploma in Reverse Engineering and Malware Intelligence - Google",
+    description:
+      "I studied this diploma to learn more about reverse engineering and malware intelligence.",
+  },
+  {
+    date: "September 2024 - Present",
+    title:
+      "Master's Degree in Artificial Intelligence and Software Engineering - UMA",
+    description:
+      "I wanted to continue my studies and specialize in AI and software engineering.",
+  },
+  {
+    date: "September 2019 - June 2024",
+    title: "Double degree in Mathematics and Computer Science - UMA",
+    description:
+      "I was part of the first class of students to graduate from this dual degree program in Málaga. I graduated with a GPA of 7.98 in Computer Science and 7.24 in Mathematics.",
+  },
+] as const;
---
+++ src/data/projects.ts
@@ -0,0 +1,40 @@
+export const projects = [
+  {
+    title: "Text2Math",
+    description:
+      "A way to input text in LaTeX or AsciiMath and get a PNG image to share across social media.",
+    link: "https://text2math.vercel.app/",
+    github: "https://github.com/alvarosh15/text2math",
+    image: "/projects/text2math.png",
+    imageWidth: 1614,
+    imageHeight: 1046,
+  },
+  {
+    title: "CleanText and MiniMarkdown",
+    description:
+      "A minimalist Markdown web editor and my own parser for a subset of Markdown.",
+    github: "https://github.com/alvarosh15/cleantext",
+    image: "/projects/cleantext.png",
+    imageWidth: 1902,
+    imageHeight: 1227,
+  },
+  {
+    title: "Amazon Last Mile Routing Challenge Dashboard",
+    description:
+      'Full-stack application for visualizing and managing data from the "Amazon Last Mile Routing Research Challenge" dataset. My end-of-degree project marked with top marks.',
+    github: "https://github.com/alvarosh15/dashboard",
+    image: "/projects/dashboard.png",
+    imageWidth: 3248,
+    imageHeight: 1950,
+  },
+  {
+    title: "MalwareAI",
+    description:
+      "An AI-powered platform for analyzing and decompiling potentially malicious files.",
+    link: "https://malwareai.vercel.app",
+    github: "https://github.com/alvarosh15/malwareai",
+    image: "/projects/malwareai.png",
+    imageWidth: 3228,
+    imageHeight: 1948,
+  },
+] as const;
